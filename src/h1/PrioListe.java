a8382c02b6730d9d9a85bd137ecc09b1175ad401
Philscil <Philipp.Linder@web.de>
2025-12-17 16:23:24 +0100

    H1

---
+++ src/h1/PrioListe.java
@@ -0,0 +1,42 @@
+package h1;
+import java.util.ArrayList;
+
+public class PrioListe {
+    ArrayList<Patient> myList = new ArrayList<>();
+
+    public void addPatient(Patient p) {
+
+        if (myList.size() == 0) {
+            myList.add(p);
+            return;
+        }
+
+        for (int i = 0; i < myList.size(); i++) {
+            if (p.prio < myList.get(i).prio) {
+                myList.add(i, p);
+                return;
+            }
+        }
+
+        myList.add(p);
+        return;
+    }
+
+    public Patient getNextPatient() {
+
+        if (myList.size() == 0) {
+            return null;
+        }
+
+        return myList.remove(0);
+    }
+
+    public int getPosition(Patient p) {
+        for (int i = 0; i < myList.size(); i++) {
+            if (myList.get(i) == p) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
